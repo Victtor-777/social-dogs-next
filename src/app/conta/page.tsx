ed32902bb8953ef3852611304808bd8444f5c00f
Victtor777 <victtor.eng.software@gmail.com>
2024-03-24 19:28:30 -0300

    adicionando o feed da parte minha conta

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,13 +1,37 @@
+import photosGet from "@/actions/photos-get";
+import userGet from "@/actions/user-get";
+import Feed from "@/components/feed/feed";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Minha Conta",
 };
 
-export default function ContaPage() {
+export default async function ContaPage() {
+  const { data: user } = await userGet();
+  const { data } = await photosGet({ user: user?.username });
+
   return (
     <main>
-      <h1>Conta</h1>
+      {data?.length ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+          <p
+            style={{ color: "#444", fontSize: "1.25rem", marginBottom: "1rem" }}
+          >
+            Nenhuma foto encontrada.
+          </p>
+          <Link
+            style={{ display: "inline-block" }}
+            className="button"
+            href="/conta/postar"
+          >
+            Postar Foto
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
